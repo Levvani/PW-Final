b3f485374402533c9c5a15c034bf029560e28405
Levvani <levanzhvania96@gmail.com>
2025-03-15 13:31:42 +0400

    hate this too

--- tests/should-fail.spec.ts
+++ tests/should-fail.spec.ts
@@ -5,5 +5,5 @@ import { test, expect } from '@playwright/test';
   test('should not recognize text', async ({page}) => {
     await page.goto("https://pw-final-opal.vercel.app/");
     await page. waitForTimeout(2000);
-    await expect(page).toHaveURL("rgdngiedubkndb");
+    await expect(page).toHaveURL("https://pw-final-opal.vercel.app/");
   });
