13c7b87103751a3591ecbf21736f1d9aac8e8d80
Levvani <levanzhvania96@gmail.com>
2025-03-15 12:46:37 +0400

    testing pw

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
       </nav>
       
       <main className="main-content">
-        <h2>Underview</h2>
+        <h2>Overview</h2>
         
         <div className="balance-cards">
           <div className="balance-card current-balance">
