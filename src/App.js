301026bfecbd61312f0e116c8bbfd2147ea91e33
Levvani <levanzhvania96@gmail.com>
2025-03-17 20:27:28 +0400

    testing against preview env3

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
       </nav>
       
       <main className="main-content">
-        <h2>Overview - Edited</h2>
+        <h2>Overview - Edited again</h2>
         
         <div className="balance-cards">
           <div className="balance-card current-balance">
