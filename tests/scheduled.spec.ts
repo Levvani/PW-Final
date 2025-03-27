7cb79b7a864517235ea855d19fa3a5c47f64d9a1
Levvani <levanzhvania96@gmail.com>
2025-03-27 23:21:55 +0400

    add cron job3

--- tests/scheduled.spec.ts
+++ tests/scheduled.spec.ts
@@ -4,7 +4,11 @@ test("check that url is correct", async ({ page }) => {
   await page.setExtraHTTPHeaders({
     "x-vercel-protection-bypass": "ayuqGFkqUzVOMlDxBcbTxEEhzFxFwivk",
   });
-  await page.goto("/");
+  await page.goto(
+    "https://pw-final-61clpvlad-levans-projects-ecb3e91e.vercel.app"
+  );
   await page.waitForTimeout(2000);
-  await expect(page).toHaveURL("/");
+  await expect(page).toHaveURL(
+    "https://pw-final-61clpvlad-levans-projects-ecb3e91e.vercel.app"
+  );
 });
